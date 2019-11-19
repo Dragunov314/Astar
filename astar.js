3ee9d3f692dbc2ffde4e0bb77cc0eeae31f4622d
Dragunov <a22451648@gmail.com>
2019-11-18 23:28:08 -0500

    test

--- astar.js
+++ astar.js
@@ -100,6 +100,7 @@ class my_board
 
     RenderBoard()
     {
+        //
         var context = this.canvas.getContext('2d');
         context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         for(var i=0;i<this.rows;i++)
