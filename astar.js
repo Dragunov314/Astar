f946eba65570228a34275244b3157478ea4fb727
Dragunov <a22451648@gmail.com>
2019-11-18 21:01:32 -0500

    Test block

--- astar.js
+++ astar.js
@@ -5,19 +5,26 @@ mycanvas.setAttribute("width", "200");
 mycanvas.setAttribute("height","200" );
 mycanvas.setAttribute("style","border:1px solid #000000;");
 mycanvas.appendChild(document.createTextNode("Your browser does not support the canvas element."));
-document.body.appendChild(mycanvas);
+
+var cvLeft = mycanvas.offsetLeft,
+    cvTop = mycanvas.offsetTop,
+    context = mycanvas.getContext('2d'),
+    blocks = [];
 
 // Add event listener for `click` events.
-elem.addEventListener('click', function(event) {
-    var x = event.pageX - elemLeft,
-        y = event.pageY - elemTop;
+mycanvas.addEventListener('click', function(event) {
+    var x = event.pageX - cvLeft,
+        y = event.pageY - cvTop;
 
-    // Collision detection between clicked offset and element.
-    elements.forEach(function(element) {
-        if (y > element.top && y < element.top + element.height 
-            && x > element.left && x < element.left + element.width) {
-            alert('clicked an element');
-        }
-    });
 
 }, false);
+
+blocks.push({
+    width: 20,
+    height: 20,
+    top: 20,
+    left: 20,
+    border:1px
+});
+
+document.body.appendChild(mycanvas);
